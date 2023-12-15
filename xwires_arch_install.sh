8d79420b7e974cf18dd5f9d037ac61f45ec7f82b
TangledWiresYT <daniel.smith@grism.com>
2023-12-15 14:24:05 +0000

    testing something

--- xwires_arch_install.sh
+++ xwires_arch_install.sh
@@ -6,5 +6,7 @@ timedatectl
 read -p "Is the time and date correct? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 # Setup disk partitions
-read -p "Which disk should Arch Linux be installed to? " install_disk
-fdisk $install_disk
+read -p "Which disk will Arch Linux be installed to? " install_disk
+echo "If you are using UEFI, you will need to make 2 partitions, a root partition (/) and an EFI System Partition (/boot)"
+read -n1 -s -r -p "Press any key to continue... " disk_continue
+cfdisk $install_disk
