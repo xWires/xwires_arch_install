fdda0859585ed1a4159164cf29f189c30015eeb8
TangledWiresYT <daniel.smith@grism.com>
2023-12-15 14:11:05 +0000

    Start to add disk stuff

--- xwires_arch_install.sh
+++ xwires_arch_install.sh
@@ -5,3 +5,6 @@ loadkeys uk
 timedatectl
 read -p "Is the time and date correct? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
+# Setup disk partitions
+read -p "Which disk should Arch Linux be installed to? " install_disk
+fdisk $install_disk
